c70128497cbff58f4959784d53564e1ca59fcf0d
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-04-18 19:24:48 -0400

    Container added

--- src/App.js
+++ src/App.js
@@ -1,28 +1,44 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 import './App.css';
 
 class App extends Component {
+
+ constructor(props) {
+    super(props);
+    this.state = {value: ''};
+
+    // this.handleChange = this.handleChange.bind(this);
+    
+  }
+
+  handleChange=(event)=> {
+    this.setState({value: event.target.value});
+  }
+
+  handleSubmit(event) {
+    alert('A name was submitted: ' + this.state.value);
+    event.preventDefault();
+  }
+  
   render() {
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App-header">
+          <div className = "Auth-main-container">
+            {/* <form onSubmit={this.handleSubmit}>
+              <label>
+                Name:
+                <input type="text" value={this.state.value} onChange={this.handleChange} />
+              </label>
+              <input type="submit" value= "Submit"/>
+            </form> */}
+          </div>
+       </div> 
       </div>
+      
     );
   }
 }
 
 export default App;
+
