aaa7226d94861b7b9c4f3d40d543236f17584de5
kartikmittal33 <mittal38@purdue.edu>
2019-04-19 18:55:46 -0400

    CreateUser and Sign in to firebase done

--- src/App.js
+++ src/App.js
@@ -1,47 +1,74 @@
-import React, { Component } from 'react';
+import React, {Component} from 'react';
 import './App.css';
-import myImage from "./basketball.png"
-import Sky from 'react-sky';
 import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Paper from '@material-ui/core/Paper';
 import Button from '@material-ui/core/Button';
-import {auth, db} from './base';
+import {auth} from './base';
 
 class App extends Component {
 
-	state = {
-		email: '',
-		password: '',
-	};
+    state = {
+        email: '',
+        password: '',
+    };
 
-	render() {
-		return (
-			<div>
-				<Paper className="paper">
+    createUser = (email, password) => {
+        auth.createUserWithEmailAndPassword(email, password)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            });
 
-					<FormControl margin="normal" required fullWidth>
-						<InputLabel htmlFor="email"> Email Address </InputLabel>
-						<Input id="email" name="email" autoComplete="email" autoFocus
-				            onChange={event => this.setState({email: event.target.value})}/>
-					</FormControl>
+    };
 
-					<FormControl margin="normal" required fullWidth>
-						<InputLabel htmlFor="password"> Password </InputLabel>
-						<Input name="password" type="password" id="password" autoComplete="current-password"
-							onChange={event => this.setState({password: event.target.value})}/>
-					</FormControl>
+    signIn = (email, password) => {
+        auth.signInWithEmailAndPassword(email, password)
+            .then((response) => {
+                console.log(response)
+            })
+            .catch(error => {
+                console.log(error)
+            });
+    };
 
-					<div className="buttonSeparator"/>
 
-					<Button fullWidth variant="contained" color="primary"
-					        onClick={() => { console.log(this.state.email + " " + this.state.password); }}> Sign in </Button>
+    render() {
+        return (
+            <div>
+                <Paper className="paper">
 
-				</Paper>
-			</div>
-		);
-	}
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="email"> Email Address </InputLabel>
+                        <Input id="email" name="email" autoComplete="email" autoFocus
+                               onChange={event => {
+                                   this.setState({email: event.target.value});
+                               }}/>
+                    </FormControl>
+
+                    <FormControl margin="normal" required fullWidth>
+                        <InputLabel htmlFor="password"> Password </InputLabel>
+                        <Input name="password" type="password" id="password" autoComplete="current-password"
+                               onChange={event => {
+                                   this.setState({password: event.target.value});
+                               }}/>
+                    </FormControl>
+
+                    <div className="buttonSeparator"/>
+
+                    <Button fullWidth variant="contained" color="primary"
+                            onClick={() => this.signIn(this.state.email, this.state.password)}> Log in </Button>
+                    <div className="buttonSeparator"/>
+                    <Button fullWidth variant="contained" color="primary"
+                            onClick={() => this.createUser(this.state.email, this.state.password)}> Sign up </Button>
+                </Paper>
+            </div>
+        );
+
+    }
 }
 
 export default App;
