474c59719055b2397b513c6ec91a619701c2cf54
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-04-21 00:40:07 -0400

    tab views

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,97 +1,81 @@
-import React, {Component} from 'react';
-import './Dashboard.css';
-import Paper from '@material-ui/core/Paper';
-import AppBar from '@material-ui/core/AppBar'
-import Button from '@material-ui/core/Button';
-import Typography from '@material-ui/core/Typography';
-import Toolbar from '@material-ui/core/Toolbar';
-import Grid from '@material-ui/core/Grid';
-import Tabs from '@material-ui/core/Tabs';
-import Tab from '@material-ui/core/Tab';
-
+import React, { Component } from "react";
+import "./Dashboard.css";
+import Paper from "@material-ui/core/Paper";
+import AppBar from "@material-ui/core/AppBar";
+import Button from "@material-ui/core/Button";
+import Typography from "@material-ui/core/Typography";
+import Toolbar from "@material-ui/core/Toolbar";
+import Grid from "@material-ui/core/Grid";
+import Tabs from "@material-ui/core/Tabs";
+import Tab from "@material-ui/core/Tab";
 
 class Dashboard extends Component {
+  state = {
+    value: 0
+  };
 
-	state = {
-        value: 0,
-	};
-
-    handleChange = (event, value) => {
+  handleChange = (event, value) => {
     this.setState({ value });
-    };
+  };
 
-	render() {
-		return(
-	        <div >
-	            <AppBar position="static" style={{ background: "#7D19E5" }}>
-	                <Toolbar>
-                        <Grid
-                            justify="space-between" 
-                            container 
-                            spacing={24}
-                        >
+  render() {
+    return (
+      <div>
+        <AppBar position="static" style={{ background: "#7D19E5" }}>
+          <Toolbar>
+            <Grid justify="space-between" container spacing={24}>
+              <Grid item>
+                <Typography variant="h6" color="inherit" align="left">
+                  SportMate
+                </Typography>
+              </Grid>
 
-                        <Grid item>
-                            <Typography variant="h6" color="inherit" align="left">
-                                SportMate
-                            </Typography>
-                        </Grid> 
-                          
-                           <Grid item>
-                            <Button color="inherit">Logout</Button>
-                           </Grid>
-                           
-	                   </Grid>
-	                </Toolbar>
-	            </AppBar>
+              <Grid item>
+                <Button color="inherit">Logout</Button>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
 
-                <div style = {styles.card}>
-                    <Paper rounded elevation2>
-                        <AppBar position="static">
-                            <Tabs variant="fullWidth" value={this.state.value} onChange={this.handleChange} style={{ background: "#7D19E5" }}>
-                                <Tab label="You created" />
-                                <Tab label="You joined" />
-                            </Tabs>
-                        </AppBar>
-                    </Paper>
-                </div>
+        <div style={styles.card}>
+          <Paper rounded elevation2>
+            <AppBar position="static">
+              <Tabs
+                variant="fullWidth"
+                value={this.state.value}
+                onChange={this.handleChange}
+                style={{ background: "#7D19E5" }}
+              >
+                <Tab label="You created" />
+                <Tab label="You joined" />
+              </Tabs>
+            </AppBar>
+            {this.state.value === 0 && <div>Hey1</div>}
+            {this.state.value === 1 && <div>Hey2</div>}
+          </Paper>
+        </div>
 
-                <div style = {styles.card}>
-                    <Paper rounded elevation2>
-                        <div>Hey</div>
-                    </Paper>
-                </div>
-
-
-
-		    </div>
-		);
-	}
+        <div style={styles.card}>
+          <Paper rounded elevation2>
+            <div>Hey</div>
+          </Paper>
+        </div>
+      </div>
+    );
+  }
 }
 
 const styles = {
-
- card:{
-    marginTop: 20, 
-    marginLeft: 'auto',
-    marginRight: 'auto',
-    width: 1200,
- },   
- 
- bar:{
-     backgroundColor: "#7D19E5",
- },
- root: {
-    flex: 3,
-  },
-  grow: {
-    flex: 1,
-  },
-  menuButton: {
-    marginLeft: -12,
-    marginRight: 20,
+  card: {
+    marginTop: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 1200
   },
 
+  bar: {
+    backgroundColor: "#7D19E5"
+  }
 };
 
 export default Dashboard;
