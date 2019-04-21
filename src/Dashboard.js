9fcfc1ab0a897e41ab1219ff437ce9d13c4d3e46
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-04-20 22:53:45 -0400

    made a bar

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -4,10 +4,13 @@ import FormControl from '@material-ui/core/FormControl';
 import InputLabel from '@material-ui/core/InputLabel';
 import Input from '@material-ui/core/Input';
 import Paper from '@material-ui/core/Paper';
-import Button from '@material-ui/core/Button';
 import {auth} from './base';
 import logo from './assets/SportMate Transparent Icon.png';
 import axios from 'axios'
+import AppBar from '@material-ui/core/AppBar'
+import Button from '@material-ui/core/Button';
+import Typography from '@material-ui/core/Typography';
+import Toolbar from '@material-ui/core/Toolbar';
 
 class Dashboard extends Component {
 
@@ -18,17 +21,41 @@ class Dashboard extends Component {
 
 	render() {
 		return(
-			<div>
-				<Paper>
-					<h1>Hey</h1>
-				</Paper>
-			</div>
+			
+				
+                    <div >
+                        <AppBar position="static">
+                            <Toolbar>
+                                <div className={styles.bar}>
+                                <Typography variant="h6" color="inherit" className={styles.grow}>
+                                    Dashboard
+                                </Typography>
+                                <Button color="inherit">Logout</Button>
+                                </div>
+                            </Toolbar>
+                        </AppBar>
+				    </div>
+                
+			
 		);
 	}
 }
 
 const styles = {
-
+ bar: {
+     flex: 1,
+     flexDirection: 'row',
+ },
+ root: {
+    flex: 3,
+  },
+  grow: {
+    flex: 1,
+  },
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20,
+  },
 
 };
 
