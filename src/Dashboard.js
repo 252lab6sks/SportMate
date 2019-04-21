5725a648fcbafbd053e3c8c69db2a15bdde87e53
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-04-21 19:55:09 -0400

    created separate component for tables and added tables to tabs

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -8,25 +8,8 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import Table from "@material-ui/core/Table";
-import TableBody from "@material-ui/core/TableBody";
-import TableCell from "@material-ui/core/TableCell";
-import TableHead from "@material-ui/core/TableHead";
-import TableRow from "@material-ui/core/TableRow";
+import TableView from './TableView'
 
-const rows = [
-  createData("Frozen yoghurt", 159, 6.0, 24, 4.0),
-  createData("Ice cream sandwich", 237, 9.0, 37, 4.3),
-  createData("Eclair", 262, 16.0, 24, 6.0),
-  createData("Cupcake", 305, 3.7, 67, 4.3),
-  createData("Gingerbread", 356, 16.0, 49, 3.9)
-];
-
-let id = 0;
-function createData(name, calories, fat, carbs, protein) {
-  id += 1;
-  return { id, name, calories, fat, carbs, protein };
-}
 
 class Dashboard extends Component {
   state = {
@@ -115,6 +98,9 @@ class Dashboard extends Component {
                 <Tab label="You joined" />
               </Tabs>
             </AppBar>
+
+            {this.state.value === 0 && <TableView/>}
+        {this.state.value === 1 && <TableView/>}
           </Paper>
         </div>
 
@@ -135,74 +121,7 @@ class Dashboard extends Component {
                   </Grid>
                 </Grid>
               </div>
-              <Table className={styles.table}>
-                <TableHead>
-                  <TableRow style={{ backgroundColor: "black", height: 35 }}>
-                    <TableCell
-                      style={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: 20
-                      }}
-                    >
-                      Sport
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: 20
-                      }}
-                      align="right"
-                    >
-                      Location
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: 20
-                      }}
-                      align="right"
-                    >
-                      Capacity
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: 20
-                      }}
-                      align="right"
-                    >
-                      Time
-                    </TableCell>
-                    <TableCell
-                      style={{
-                        color: "white",
-                        fontWeight: "bold",
-                        fontSize: 20
-                      }}
-                      align="right"
-                    >
-                      Host
-                    </TableCell>
-                  </TableRow>
-                </TableHead>
-                <TableBody>
-                  {rows.map(row => (
-                    <TableRow key={row.id} style={{ backgroundColor: "white", height: 35 }}> 
-                      <TableCell component="th" scope="row">
-                        {row.name}
-                      </TableCell>
-                      <TableCell align="right">{row.calories}</TableCell>
-                      <TableCell align="right">{row.fat}</TableCell>
-                      <TableCell align="right">{row.carbs}</TableCell>
-                      <TableCell align="right">{row.protein}</TableCell>
-                    </TableRow>
-                  ))}
-                </TableBody>
-              </Table>
+              <TableView />
             </AppBar>
           </Paper>
         </div>
