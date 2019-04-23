49615aa6ca059c169370794283b56618e46488b3
Shiv Paul <shivapaul98@gmail.com>
2019-04-23 15:26:30 -0400

    Update TableView.js

--- src/TableView.js
+++ src/TableView.js
@@ -8,8 +8,14 @@ import Grid from "@material-ui/core/Grid";
 
 let id = 0;
 
-function createData(sport, location, capacity, time, host, people) {
+function createData(obj) {
 	id += 1;
+	let sport = obj.sport;
+	let location = obj.location;
+	let capacity = obj.capacity;
+	let time = obj.time;
+	let host = obj.host;
+	let people = obj.people;
 	return { id, sport, location, capacity, time, host, people };
 }
 
@@ -17,7 +23,6 @@ class TableView extends Component {
 	state = {};
 
 	getTableComponent(row) {
-
 		return (
 			<ExpansionPanel style={{ width: 900 }}>
 				<ExpansionPanelSummary>
@@ -55,7 +60,7 @@ class TableView extends Component {
 					</Grid>
 				</ExpansionPanelSummary>
 				<ExpansionPanelDetails>
-					{this.getPeople(row.people)};
+					{this.getPeople(row.people)}
 				</ExpansionPanelDetails>
 			</ExpansionPanel>
 		);
@@ -80,98 +85,33 @@ class TableView extends Component {
 			if (this.props.type == 'created') {
 				Object.keys(this.props.events).forEach((key, index) => {
 					if (this.props.events[key].host == this.props.email) {
-						let row = createData(
-							this.props.events[key].sport,
-							this.props.events[key].location,
-							this.props.events[key].capacity,
-							this.props.events[key].time,
-							this.props.events[key].host,
-							this.props.events[key].people,
-						);
+						let row = createData(this.props.events[key]);
 						rows.push(row);
 					}
 				}
 				);
-
 			} 
 			else if (this.props.type == 'joined') {
 				Object.keys(this.props.events).forEach((key, index) => {
 					Object.keys(this.props.events[key].people).forEach((key_child, index_child) => {
 						if (this.props.events[key].people[key_child] == this.props.email &&
 							this.props.events[key].host != this.props.email) {
-							let row = createData(
-								this.props.events[key].sport,
-								this.props.events[key].location,
-								this.props.events[key].capacity,
-								this.props.events[key].time,
-								this.props.events[key].host,
-								this.props.events[key].people,
-							);
+							let row = createData(this.props.events[key]);
 							rows.push(row);
 						}
 					}
 					);
 				}
 				);
-
 			} 
 			else {
 				Object.keys(this.props.events).forEach((key, index) => {
-					let row = createData(
-						this.props.events[key].sport,
-						this.props.events[key].location,
-						this.props.events[key].capacity,
-						this.props.events[key].time,
-						this.props.events[key].host,
-						this.props.events[key].people,
-					);
+					let row = createData(this.props.events[key]);
 					rows.push(row);
-				}
-				);
-
+				});
 			}
 
-			return (rows.map(row => (
-				<ExpansionPanel style={{ width: 900 }}>
-					<ExpansionPanelSummary>
-						<Grid
-							justify="space-between"
-							alignItems="left"
-							container
-							spacing={24}>
-
-							<Grid item>
-								<Typography align="left">
-									{row.sport}
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography align="left">
-									{row.location}
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography align="left">
-									{row.capacity}
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography align="left">
-									{row.time}
-								</Typography>
-							</Grid>
-							<Grid item>
-								<Typography align="left">
-									{row.host}
-								</Typography>
-							</Grid>
-						</Grid>
-					</ExpansionPanelSummary>
-					<ExpansionPanelDetails>
-						{this.getPeople(row.people)}
-					</ExpansionPanelDetails>
-				</ExpansionPanel>
-			)));
+			return (rows.map(row => this.getTableComponent(row)));
 		}
 	}
 
@@ -219,18 +159,5 @@ class TableView extends Component {
 		);
 	};
 }
-
-const styles = {
-	table: {
-		minWidth: 700
-	},
-
-	card: {
-		marginTop: 20,
-		marginLeft: "auto",
-		marginRight: "auto",
-		width: 900
-	}
-};
 
 export default TableView;
