4e42199c2ba9299a40b79de7d65bf6f2793d88a5
Sehajbir Randhawa <srandhaw@purdue.edu>
2019-04-23 23:28:34 -0400

    listeners added

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -7,216 +7,252 @@ import Toolbar from "@material-ui/core/Toolbar";
 import Grid from "@material-ui/core/Grid";
 import Tabs from "@material-ui/core/Tabs";
 import Tab from "@material-ui/core/Tab";
-import TableView from './TableView';
-import AddEventModal from './AddEventModal.js'
-import { db,functions } from './base';
+import TableView from "./TableView";
+import AddEventModal from "./AddEventModal.js";
+import { db, functions } from "./base";
 
 class Dashboard extends Component {
+  state = {
+    email: "",
+    uid: "",
+    value: 0,
+    addEventState: false,
+    sport: "",
+    location: "",
+    capacity: "",
+    time: "",
+    host: "",
+    events: []
+  };
 
-	state = {
-		email: '',
-		uid: '',
-		value: 0,
-		addEventState: false,
-		sport: '',
-		location: '',
-		capacity: '',
-		time: '',
-		host: '',
-		events: [],
-	};
+  componentWillMount() {
+    let email = localStorage.getItem("email");
+    let uid = localStorage.getItem("uid");
 
-	componentWillMount() {
-		let email = localStorage.getItem('email');
-		let uid = localStorage.getItem('uid');
+    this.setState({
+      email: email,
+      uid: uid
+    });
 
-		this.setState({
-			email: email,
-			uid: uid,
-		});
+    db.ref("/").on("child_changed", snapshot => {
+      try {
+        var getData = functions.httpsCallable("dataGet");
+        getData().then(result => {
+          this.setState({
+            events: result.data.events
+          });
+        });
+      } catch (error) {
+        console.log("error " + error);
+      }
+    });
 
-		// db.ref("events/").once('value').then((snapshot) => {
-		// 	this.setState({
-		// 		events: snapshot.val(),
-		// 	});
-		// });
-		// var events;
+    try {
+      var getData = functions.httpsCallable("dataGet");
+      getData().then(result => {
+        this.setState({
+          events: result.data.events
+        });
+      });
+    } catch (error) {
+      console.log("error " + error);
+    }
+  }
 
-		try{
-		var getData = functions.httpsCallable('dataGet');
-		getData().then((result)=> {
-			
-			this.setState({
-					events: result.data.events,
-			});
-		
-		})
+  handleChange = (event, value) => {
+    this.setState({ value: value });
+  };
 
+  addEventOpen = () => {
+    this.setState({ addEventState: true });
+  };
 
-		}catch(error){
-			console.log("error "+error)
-		}
-	}
+  addEventClose = () => {
+    this.setState({ addEventState: false });
+  };
 
-	handleChange = (event, value) => {
-		this.setState({ value: value });
-	};
+  eventSubmit = () => {
+    var ref = db.ref("events/").push();
+    var eventID = ref.key;
 
-	addEventOpen = () => {
-		this.setState({ addEventState: true });
-	};
+    db.ref("events/" + eventID + "/")
+      .set({
+        sport: this.state.sport,
+        location: this.state.location,
+        capacity: this.state.capacity,
+        time: this.state.time,
+        people: { "1": `${this.state.email}` },
+        host: this.state.email
+      })
+      .then(data => {
+        console.log("Added to db");
+        this.addEventClose();
+      })
+      .catch(error => console.log(error));
+  };
 
-	addEventClose = () => {
-		this.setState({ addEventState: false });
-	};
+  render() {
+    return (
+      <div>
+        <AppBar position="static" style={{ background: "#7D19E5" }}>
+          <Toolbar>
+            <Grid justify="space-between" container spacing={24}>
+              <Grid item>
+                <Typography variant="h6" color="inherit" align="left">
+                  SportMate
+                </Typography>
+              </Grid>
 
-	eventSubmit = () => {
-		var ref = db.ref("events/").push();
-		var eventID = ref.key;
+              <Grid item>
+                <Button
+                  style={{ color: "#7D19E5", backgroundColor: "#FCD704" }}
+                  color="inherit"
+                  onClick={() => {
+                    localStorage.setItem("loggedIn", "false");
+                    this.setState({
+                      email: "",
+                      uid: ""
+                    });
+                    this.props.SignInHandler(this.state);
+                  }}
+                >
+                  Logout
+                </Button>
+              </Grid>
+            </Grid>
+          </Toolbar>
+        </AppBar>
 
-		db.ref("events/" + eventID + "/").set({
-			sport: this.state.sport,
-			location: this.state.location,
-			capacity: this.state.capacity,
-			time: this.state.time,
-			people: { "1": `${this.state.email}` },
-			host: this.state.email
+        <div style={styles.card}>
+          <AppBar position="static">
+            <div style={{ backgroundColor: "#7D19E5" }}>
+              <Grid
+                justify="space-between"
+                alignItems="center"
+                container
+                spacing={24}
+              >
+                <Grid item style={{ margin: 10, marginLeft: 20 }}>
+                  <Typography variant="h6" color="inherit" align="left">
+                    Your Events
+                  </Typography>
+                </Grid>
 
-		}).then((data) => {
-			console.log("Added to db");
-			this.addEventClose();
-		}).catch((error) => console.log(error));
-	};
+                <Grid
+                  item
+                  style={{
+                    marginTop: "auto",
+                    marginBottom: "auto",
+                    marginRight: 20
+                  }}
+                >
+                  <Button
+                    style={{ color: "#7D19E5", backgroundColor: "#FCD704" }}
+                    color="inherit"
+                    onClick={() => {
+                      this.addEventOpen();
+                    }}
+                  >
+                    Add Event
+                  </Button>
+                </Grid>
+              </Grid>
+            </div>
+            <Tabs
+              variant="fullWidth"
+              value={this.state.value}
+              onChange={this.handleChange}
+              style={{ background: "#7D19E5" }}
+            >
+              <Tab label="You created" />
+              <Tab label="You joined" />
+            </Tabs>
+          </AppBar>
+          {this.state.value === 0 && (
+            <TableView
+              events={this.state.events}
+              type="created"
+              email={this.state.email}
+            />
+          )}
+          {this.state.value === 1 && (
+            <TableView
+              events={this.state.events}
+              type="joined"
+              email={this.state.email}
+            />
+          )}
+        </div>
 
-	render() {
-		return (
-			<div>
-				<AppBar position="static" style={{ background: "#7D19E5" }}>
-					<Toolbar>
-						<Grid
-							justify="space-between"
-							container
-							spacing={24}>
+        <div style={styles.card}>
+          <AppBar position="static">
+            <div style={{ backgroundColor: "#7D19E5" }}>
+              <Grid
+                justify="space-between"
+                alignItems="center"
+                container
+                spacing={24}
+              >
+                <Grid item style={{ margin: 10, marginLeft: 20 }}>
+                  <Typography variant="h6" color="inherit" align="left">
+                    All Events
+                  </Typography>
+                </Grid>
+              </Grid>
+            </div>
+          </AppBar>
 
-							<Grid item>
-								<Typography variant="h6" color="inherit" align="left">
-									SportMate
-								</Typography>
-							</Grid>
+          <TableView
+            events={this.state.events}
+            type="all"
+            email={this.state.email}
+          />
+        </div>
 
-							<Grid item>
-								<Button style={{ color: "#7D19E5", backgroundColor: "#FCD704" }} color="inherit"
-									onClick={() => {
-										localStorage.setItem('loggedIn', 'false');
-										this.setState({
-											email: '',
-											uid: ''
-										});
-										this.props.SignInHandler(this.state);
-									}}>
-									Logout</Button>
-							</Grid>
-
-						</Grid>
-					</Toolbar>
-				</AppBar>
-
-				<div style={styles.card}>
-					<AppBar position="static">
-						<div style={{ backgroundColor: "#7D19E5" }}>
-							<Grid
-								justify="space-between"
-								alignItems="center"
-								container
-								spacing={24}>
-
-								<Grid item style={{ margin: 10, marginLeft: 20 }}>
-									<Typography variant="h6" color="inherit" align="left">
-										Your Events
-										</Typography>
-								</Grid>
-
-								<Grid item style={{ marginTop: "auto", marginBottom: "auto", marginRight: 20 }}>
-									<Button style={{ color: "#7D19E5", backgroundColor: "#FCD704" }} color="inherit"
-										onClick={() => {
-											this.addEventOpen()
-										}}>Add Event</Button>
-								</Grid>
-							</Grid>
-						</div>
-						<Tabs variant="fullWidth" value={this.state.value} onChange={this.handleChange}
-							style={{ background: "#7D19E5" }}>
-							<Tab label="You created" />
-							<Tab label="You joined" />
-						</Tabs>
-					</AppBar>
-					{this.state.value === 0 &&
-						<TableView events={this.state.events} type='created' email={this.state.email} />}
-					{this.state.value === 1 &&
-						<TableView events={this.state.events} type='joined' email={this.state.email} />}
-				</div>
-
-				<div style={styles.card}>
-					<AppBar position="static">
-						<div style={{ backgroundColor: "#7D19E5" }}>
-							<Grid
-								justify="space-between"
-								alignItems="center"
-								container
-								spacing={24}>
-
-								<Grid item style={{ margin: 10, marginLeft: 20 }}>
-									<Typography variant="h6" color="inherit" align="left">
-										All Events
-									</Typography>
-								</Grid>
-							</Grid>
-						</div>
-					</AppBar>
-
-					<TableView events={this.state.events} type='all' email={this.state.email} />
-				</div>
-
-				<AddEventModal details={this.state} addEventOpen={this.addEventOpen} addEventClose={this.addEventClose} />
-			</div>
-		);
-	}
+        <AddEventModal
+          details={this.state}
+          addEventOpen={this.addEventOpen}
+          addEventClose={this.addEventClose}
+        />
+      </div>
+    );
+  }
 }
 
 const styles = {
-	table: {
-		minWidth: 700
-	},
+  table: {
+    minWidth: 700
+  },
 
-	card: {
-		marginTop: 20,
-		marginLeft: 'auto',
-		marginRight: 'auto',
-		width: 900,
-	},
+  card: {
+    marginTop: 20,
+    marginLeft: "auto",
+    marginRight: "auto",
+    width: 900
+  },
 
-	bar: {
-		backgroundColor: "#7D19E5",
-	},
+  bar: {
+    backgroundColor: "#7D19E5"
+  },
 
-	root: {
-		flex: 3,
-	},
+  root: {
+    flex: 3
+  },
 
-	grow: {
-		flex: 1,
-	},
+  grow: {
+    flex: 1
+  },
 
-	menuButton: {
-		marginLeft: -12,
-		marginRight: 20,
-	},
+  menuButton: {
+    marginLeft: -12,
+    marginRight: 20
+  },
 
-	addEventInputStyle: {
-		marginTop: 30,
-		marginLeft: 15,
-		marginRight: 15,
-	}
+  addEventInputStyle: {
+    marginTop: 30,
+    marginLeft: 15,
+    marginRight: 15
+  }
 };
 
 export default Dashboard;
